4cc5db1b8341909d414d87feadee41966e126aa0
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-07-02 15:34:57 -0700

    week3(viz): producing word cloud using filtered words

--- week3/tweets/data_viz_pt3.py
+++ week3/tweets/data_viz_pt3.py
@@ -9,6 +9,7 @@ https://github.com/GirlsFirst/SIP-2018-starter/blob/master/U2-Applications/U2.1-
 import json
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 DATA_FILE = "tweets_small.json"
 
@@ -69,6 +70,33 @@ def plot_histogram(data, data_bins, axis_list, title, xlabel, ylabel):
     plt.show()
 
 '''
+build_wordcloud:
+'''
+def build_wordcloud(tweets, tweet_key):
+    # 1. combine all tweets into one large string
+    all_tweets = ""
+
+    for tweet in tweets:
+        all_tweets += tweet["text"]
+
+    # 2. create new textblob
+    big_blob = TextBlob(all_tweets)
+
+    # 3. build frequency dictionary
+    word_freq = big_blob.word_counts
+    filtered = {}
+    ignored_words = ["and", "about", "http", "https", "the"]
+
+    # filter short words
+    for key in word_freq:
+        if (len(key) > 3) and key not in ignored_words:
+            filtered[key] = word_freq[key]
+
+    # 4. draw a word cloud
+    cloud = WordCloud().generate_from_frequencies(filtered)
+    return cloud
+
+'''
 main: this is our main function where everything happens
 '''
 def main():
@@ -86,23 +114,28 @@ def main():
 
     print("Average polarity: ", ave_polarity)
     print("Average subjectivity: ", ave_subjectivity)
+    #
+    # # 3. plot values in a histogram
+    # p_bins = [ (x/10) for x in range(-10, 10) ]
+    # # p_bins = [-1, -0.5, 0, 0.5, 1.0]      # much simpler bins
+    # p_limits = [-1.1, 1.1, 0, len(all_polarity)*(2/3)]
+    # plot_histogram(all_polarity, p_bins, p_limits, "Tweet Polarities", "Polarity", "Number of Tweets")
+    #
+    # # # using different histogram attributes
+    # # bins = [-1, -0.5, 0.0, 0.5, 1]      # [x-axis values]
+    # # axes = [-1.1, 1.1, 0, 90]            # [min_x, max_x, min_y, max_y]
+    # # plot_histogram(all_polarity, bins, axes, "Tweet Polarities", "Polarity", "Number of Tweets")
+    #
+    # s_bins = [ (x/10) for x in range(0, 10) ]
+    # # s_bins = [0, 0.5, 1.0]                # much simpler bins
+    # s_limits = [-0.1 , 1.1, 0, len(all_subjectivity)*(2/3)]
+    # plot_histogram(all_subjectivity, s_bins, s_limits, "Tweet Subjectivity", "Subjectivity", "Number of Tweets")
 
-    # 3. plot values in a histogram
-    p_bins = [ (x/10) for x in range(-10, 10) ]
-    # p_bins = [-1, -0.5, 0, 0.5, 1.0]      # much simpler bins
-    p_limits = [-1.1, 1.1, 0, len(all_polarity)*(2/3)]
-    plot_histogram(all_polarity, p_bins, p_limits, "Tweet Polarities", "Polarity", "Number of Tweets")
-
-    # # using different histogram attributes
-    # bins = [-1, -0.5, 0.0, 0.5, 1]      # [x-axis values]
-    # axes = [-1.1, 1.1, 0, 90]            # [min_x, max_x, min_y, max_y]
-    # plot_histogram(all_polarity, bins, axes, "Tweet Polarities", "Polarity", "Number of Tweets")
-
-    s_bins = [ (x/10) for x in range(0, 10) ]
-    # s_bins = [0, 0.5, 1.0]                # much simpler bins
-    s_limits = [-0.1 , 1.1, 0, len(all_subjectivity)*(2/3)]
-    plot_histogram(all_subjectivity, s_bins, s_limits, "Tweet Subjectivity", "Subjectivity", "Number of Tweets")
-
+    # 4. create a word cloud and display it
+    wc = build_wordcloud(tweetData, "text")
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
 
 '''
 where the program actually starts when you run it
