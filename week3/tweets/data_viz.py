33a57ebf58633f61de6da6baf071d634117649f3
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-07-02 10:54:09 -0700

    week3(viz): added data viz project starter code

---
+++ week3/tweets/data_viz.py
@@ -0,0 +1,22 @@
+'''
+In this project, you will visualize the feelings and language used in a set of
+Tweets. This starter code loads the appropriate libraries and the Twitter data you'll
+need!
+
+https://github.com/GirlsFirst/SIP-2018-starter/blob/master/U2-Applications/U2.1-Data/data_vis_project_starter.py
+'''
+
+import json
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+#Get the JSON data
+tweetFile = open("TwitterData/tweets_small.json", "r")
+tweetData = json.load(tweetFile)
+tweetFile.close()
+
+# Continue your program below!
+
+# Textblob sample:
+tb = TextBlob("You are a brilliant computer scientist.")
+print(tb.polarity)
