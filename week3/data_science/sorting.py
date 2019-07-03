8968d1462e0d529b312f97b9cc77136a3d5db77e
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-07-03 14:51:47 -0700

    week3(DS): additional sorting helper functions

--- week3/data_science/sorting.py
+++ week3/data_science/sorting.py
@@ -18,8 +18,23 @@ get_max_N: return list of top N tuples from the sorted list
 '''
 def get_max_N(tuples, N):
     start = len(tuples) - N
-    top10 = tuples[start:]
+    top10 = tuples[start:]      # create sublist containing the last N tuples
     return top10
+
+'''
+tuples_to_lists: convert a list of tuples into two lists
+    keys    :    list containing the first tuple values
+    values  :    list containing the second tuple values
+'''
+def tuples_to_lists(tuples):
+    keys = []
+    values = []
+
+    for tup in tuples:
+        keys.append(tup[0])
+        values.append(tup[1])
+
+    return (keys, values)
 
 def main():
     x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
@@ -31,6 +46,11 @@ def main():
     # get top 2 tuples
     top = get_max_N(sorted, 2)
     print(top)
+
+    # convert list of tuples into two lists
+    (list_of_keys, list_of_values) = tuples_to_lists(top)
+    print("keys: " , list_of_keys)
+    print("values: " , list_of_values)
 
 if __name__ == '__main__':
     main()
