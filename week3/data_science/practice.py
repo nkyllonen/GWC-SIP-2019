27d0614416f9ecc5e36b456cef758e040ecdeed8
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-07-03 10:45:02 -0700

    week3(DS): filling in comments

--- week3/data_science/practice.py
+++ week3/data_science/practice.py
@@ -7,7 +7,15 @@ import school_scores
 main: the function that calls other functions and gets things done
 '''
 def main():
+    # returns all of the score data as a list
     list_of_records = school_scores.get_all()
+
+    # 1. print out the first element
+
+    # 2. print out the state name and year for each row in the data set
+
+    # 3. print out the total number of test-takers for each state per year
+    
 
 if __name__ == '__main__':
     main()
