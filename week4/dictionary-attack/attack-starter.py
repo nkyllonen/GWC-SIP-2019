75c92f82c788cb9455f157fc2107f9f161de96ab
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-07-08 10:04:17 -0700

    week4(attack): updating starter code

--- week4/dictionary-attack/attack-starter.py
+++ week4/dictionary-attack/attack-starter.py
@@ -1,10 +1,16 @@
-#Opens a file. You can now look at each line in the file individually with a statement like "for line in f:
-f = open("dictionary.txt","r")
+FILE = "dictionary.txt"
 
-print("Can your password survive a dictionary attack?")
+def main():
+    # Opens a file. You can now look at each line in the file individually with a statement like "for line in f:"
+    f = open(FILE,"r")
 
-#Take input from the keyboard, storing in the variable test_password
-#NOTE - You will have to use .strip() to strip whitespace and newlines from the file and passwords
-test_password = input("Type in a trial password: ")
+    print("Can your password survive a dictionary attack?")
 
-#Write logic to see if the password is in the dictionary file below here:
+    # Take input from the keyboard, storing in the variable test_password
+    # NOTE - You will have to use .strip() to strip whitespace and newlines from the file and passwords
+    test_password = input("Type in a trial password: ")
+
+    # Write logic to see if the password is in the dictionary file below here:
+
+if __name__ == '__main__':
+    main()
