6c15a4643a59a103478c41b303574c951c139887
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-07-11 14:02:54 -0700

    week4(api): starting to get country name

--- week4/maps/mymaps.js
+++ week4/maps/mymaps.js
@@ -35,5 +35,33 @@ function panHome() {
   });
 }
 
+// pan to a given location
+function panToLocation() {
+  // get value from textarea element
+  var countryName = document.getElementById("country-name").value;
+
+  // check that there is something there
+  if (countryName === "") {
+    alert("You forgot to enter a country!");
+    return;
+  }
+
+  console.log("country name: " + countryName);
+
+  // build Countries API query
+  var query = "https://restcountries.eu/rest/v2/name/" + countryName;
+
+  // assume that we have our data
+  var lon = 0.0;
+  var lat = 0.0;
+
+  var location = ol.proj.fromLonLat([lon, lat]);
+
+  view.animate({
+    center: location, // our "home" location
+    duration: 2000       // in milliseconds
+  });
+}
+
 // run init when the window loads
 window.onload = init;
